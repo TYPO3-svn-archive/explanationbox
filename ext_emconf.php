fecb5bfc4ca422ed1a43eec3fbc1178655a47cb3
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-06 22:06:57 +0000

    add locallang hashes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/explanationbox/trunk@13854 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "explanationbox"
 #
-# Auto generated 06-11-2008 21:29
+# Auto generated 06-11-2008 23:06
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -42,6 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"f2e6";s:12:"ext_icon.gif";s:4:"d085";s:17:"ext_localconf.php";s:4:"d1c9";s:14:"ext_tables.php";s:4:"dcbf";s:14:"ext_tables.sql";s:4:"181c";s:35:"icon_tx_explanationbox_sections.gif";s:4:"475a";s:13:"locallang.xml";s:4:"d41d";s:16:"locallang_db.xml";s:4:"7a99";s:7:"tca.php";s:4:"cb1b";s:31:"configuration/tsconfig_page.txt";s:4:"1c3f";s:19:"doc/wizard_form.dat";s:4:"81c4";s:20:"doc/wizard_form.html";s:4:"9cd8";s:35:"pi1/class.tx_explanationbox_pi1.php";s:4:"d301";}',
 );
 
 ?>
