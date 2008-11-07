a1f303046442181da735c15ad1b4b2bb1bdbe405
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-07 21:53:02 +0000

    Update hashes.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/explanationbox/trunk@13921 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "explanationbox"
 #
-# Auto generated 06-11-2008 23:06
+# Auto generated 07-11-2008 22:52
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"f2e6";s:12:"ext_icon.gif";s:4:"d085";s:17:"ext_localconf.php";s:4:"d1c9";s:14:"ext_tables.php";s:4:"dcbf";s:14:"ext_tables.sql";s:4:"181c";s:35:"icon_tx_explanationbox_sections.gif";s:4:"475a";s:13:"locallang.xml";s:4:"d41d";s:16:"locallang_db.xml";s:4:"7a99";s:7:"tca.php";s:4:"cb1b";s:31:"configuration/tsconfig_page.txt";s:4:"1c3f";s:19:"doc/wizard_form.dat";s:4:"81c4";s:20:"doc/wizard_form.html";s:4:"9cd8";s:35:"pi1/class.tx_explanationbox_pi1.php";s:4:"d301";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"f2e6";s:31:"class.tx_explanationbox_tca.php";s:4:"9e72";s:12:"ext_icon.gif";s:4:"d085";s:17:"ext_localconf.php";s:4:"74bd";s:14:"ext_tables.php";s:4:"1e7c";s:14:"ext_tables.sql";s:4:"181c";s:35:"icon_tx_explanationbox_sections.gif";s:4:"475a";s:13:"locallang.xml";s:4:"7623";s:16:"locallang_db.xml";s:4:"7a99";s:7:"tca.php";s:4:"011e";s:27:"configuration/constants.txt";s:4:"3e5b";s:23:"configuration/setup.txt";s:4:"8100";s:31:"configuration/tsconfig_page.txt";s:4:"1c3f";s:35:"pi1/class.tx_explanationbox_pi1.php";s:4:"35a6";s:17:"pi1/locallang.xml";s:4:"286f";s:29:"pi1/tx_explanationbox_pi1.css";s:4:"8fc9";s:30:"pi1/tx_explanationbox_pi1.html";s:4:"8424";s:28:"pi1/tx_explanationbox_pi1.js";s:4:"e36b";s:37:"pi1/contrib/mootools-1.2.1-core-yc.js";s:4:"cad2";s:25:"pi1/images/pfeil_link.gif";s:4:"31b8";s:30:"pi1/images/pfeil_link_left.gif";s:4:"e177";s:24:"pi1/images/pipe_grey.gif";s:4:"ff75";}',
 );
 
 ?>
