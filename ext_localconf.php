9fbc814e33a364f5b68f4ccd792a67ee595c2eac
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-13 10:41:06 +0000

    Remove the configuration for displaying any columns since we do not need to modify it anyway.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/explanationbox/trunk@14091 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,10 +9,6 @@ t3lib_extMgm::addPItoST43(
 	$_EXTKEY, 'pi1/class.tx_explanationbox_pi1.php', '_pi1', 'CType', 1
 );
 
-t3lib_extMgm::addPageTSConfig(
-	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:explanationbox/configuration/tsconfig_page.txt">'
-);
-
 // Adds our custom function to a hook in t3lib/class.t3lib_tcemain.php
 // Used for post-validation of fields in back-end forms.
 $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
