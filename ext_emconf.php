b6657ac252dc8ca19b9d3e6ee69fcc6d5727b7c6
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-04 23:42:51 +0000

    version 0.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/explanationbox/trunk@14606 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "explanationbox"
 #
-# Auto generated 24-11-2008 00:19
+# Auto generated 05-12-2008 00:29
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'oliverklee.de',
-	'version' => '0.2.0',
+	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
@@ -46,7 +46,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"37a2";s:31:"class.tx_explanationbox_tca.php";s:4:"78de";s:12:"ext_icon.gif";s:4:"d085";s:17:"ext_localconf.php";s:4:"9c3c";s:14:"ext_tables.php";s:4:"75a4";s:14:"ext_tables.sql";s:4:"ad55";s:34:"icon_tx_explanationbox_columns.gif";s:4:"475a";s:35:"icon_tx_explanationbox_sections.gif";s:4:"475a";s:13:"locallang.xml";s:4:"7623";s:16:"locallang_db.xml";s:4:"6474";s:7:"tca.php";s:4:"b51d";s:27:"configuration/constants.txt";s:4:"3e5b";s:23:"configuration/setup.txt";s:4:"8100";s:14:"pi1/ce_wiz.gif";s:4:"19d0";s:35:"pi1/class.tx_explanationbox_pi1.php";s:4:"fca9";s:43:"pi1/class.tx_explanationbox_pi1_wizicon.php";s:4:"538e";s:17:"pi1/locallang.xml";s:4:"286f";s:29:"pi1/tx_explanationbox_pi1.css";s:4:"a537";s:30:"pi1/tx_explanationbox_pi1.html";s:4:"af70";s:28:"pi1/tx_explanationbox_pi1.js";s:4:"8b84";s:37:"pi1/contrib/mootools-1.2.1-core-yc.js";s:4:"cad2";s:25:"pi1/images/arrow_left.gif";s:4:"e177";s:26:"pi1/images/arrow_right.gif";s:4:"31b8";s:19:"pi1/images/pipe.gif";s:4:"ff75";}',
+	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"1bd6";s:31:"class.tx_explanationbox_tca.php";s:4:"78de";s:12:"ext_icon.gif";s:4:"d085";s:17:"ext_localconf.php";s:4:"9c3c";s:14:"ext_tables.php";s:4:"f9bc";s:14:"ext_tables.sql";s:4:"8d53";s:34:"icon_tx_explanationbox_columns.gif";s:4:"475a";s:35:"icon_tx_explanationbox_sections.gif";s:4:"475a";s:13:"locallang.xml";s:4:"7623";s:16:"locallang_db.xml";s:4:"6474";s:7:"tca.php";s:4:"c8c2";s:27:"configuration/constants.txt";s:4:"3e5b";s:23:"configuration/setup.txt";s:4:"8100";s:14:"pi1/ce_wiz.gif";s:4:"19d0";s:35:"pi1/class.tx_explanationbox_pi1.php";s:4:"a368";s:43:"pi1/class.tx_explanationbox_pi1_wizicon.php";s:4:"538e";s:17:"pi1/locallang.xml";s:4:"286f";s:29:"pi1/tx_explanationbox_pi1.css";s:4:"a537";s:30:"pi1/tx_explanationbox_pi1.html";s:4:"af70";s:28:"pi1/tx_explanationbox_pi1.js";s:4:"8b84";s:37:"pi1/contrib/mootools-1.2.1-core-yc.js";s:4:"cad2";s:25:"pi1/images/arrow_left.gif";s:4:"e177";s:26:"pi1/images/arrow_right.gif";s:4:"31b8";s:19:"pi1/images/pipe.gif";s:4:"ff75";}',
 );
 
 ?>
