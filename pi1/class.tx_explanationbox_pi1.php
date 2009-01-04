0a0f4c1c294d55193d92b0c0b6522124aecf56ab
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-04 13:04:13 +0000

    [Bug 2494] The autoloader should include the constants
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/explanationbox/trunk@15473 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_explanationbox_pi1.php
+++ pi1/class.tx_explanationbox_pi1.php
@@ -25,7 +25,6 @@
 require_once(PATH_typo3 . 'sysext/cms/tslib/class.tslib_content.php');
 
 require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
-require_once(t3lib_extMgm::extPath('oelib') . 'tx_oelib_commonConstants.php');
 
 /**
  * Content type 'Explanation box' for the 'explanationbox' extension.
