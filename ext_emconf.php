1c7abf36368f586c6f19fcb4ced9f41a3c7de44d
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-05 14:18:20 +0000

    Released oelib 0.5.2.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/explanationbox/trunk@16488 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'php' => '5.1.2-0.0.0',
 			'typo3' => '4.2.0-0.0.0',
 			'cms' => '',
-			'oelib' => '0.5.1-',
+			'oelib' => '0.5.2-',
 		),
 		'conflicts' => array(
 			'date2cal' => '',
